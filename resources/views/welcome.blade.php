5173f5a5ea3e3c5e011b8d9482f9eb79d15e1efc
YudaKusuma16 <yuda16072005@gmail.com>
2026-04-02 14:10:21 +0700

    icon mansys

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WEB HSR</title>
-    <link rel="icon" type="image/x-icon" href="/hsricon.ico">
+    <link rel="icon" type="image/x-icon" href="/images/mansys-icon.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="apple-touch-icon" href="/images/mansys-icon.png">
+    <meta name="theme-color" content="#000000">
 
     @viteReactRefresh
     @vite('resources/js/main.jsx')
