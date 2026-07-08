b9192fa3f8e87d69e2d5e71a3e55e9d33ce479fe
Aqilafaz123 <aqilafazrul1@gmail.com>
2026-07-08 18:02:29 +0700

    bug fix

--- resources/views/pdf/partials/signature_block_hsr.blade.php
+++ resources/views/pdf/partials/signature_block_hsr.blade.php
@@ -1,25 +1,16 @@
-@if(!empty($ttd_hsr) || !empty($nama_ttd_hsr))
-                    <table border="0" cellspacing="0" cellpadding="0" align="left" style="border-collapse: collapse;">
-                        @if(!empty($ttd_hsr))
-                        <tr>
-                            <td align="center" style="text-align: center; padding: 0;">
-                                <img src="{{ $ttd_hsr }}" alt="Tanda Tangan HSR" style="height: 62px; max-width: 240px; display: block; margin: 0 auto;" />
-                            </td>
-                        </tr>
-                        @endif
-                        <tr>
-                            <td align="center" style="text-align: center; padding: 0;">
-                                <p class="MsoNormal" style="margin-bottom: 0in; line-height: normal">
-                                    <span lang="EN-ID">@include('pdf.partials.signature_paren', ['name' => $nama_ttd_hsr ?? null])</span>
-                                </p>
-                            </td>
-                        </tr>
-                    </table>
-                    @else
-                    <p class="MsoNormal" style="margin-bottom: 0in; line-height: normal"><span lang="EN-ID">&nbsp;</span></p>
-                    <p class="MsoNormal" style="margin-bottom: 0in; line-height: normal"><span lang="EN-ID">&nbsp;</span></p>
-                    <p class="MsoNormal" style="margin-bottom: 0in; line-height: normal"><span lang="EN-ID">&nbsp;</span></p>
-                    <p class="MsoNormal" style="margin-bottom: 0in; line-height: normal">
-                        <span lang="EN-ID">@include('pdf.partials.signature_paren', ['name' => $nama_ttd_hsr ?? null])</span>
-                    </p>
-                    @endif
+<table border="0" cellspacing="0" cellpadding="0" style="width: 200px; margin: 0; border-collapse: collapse;">
+    <tr>
+        <td align="center" style="height: 62px; padding: 0; text-align: center; vertical-align: bottom;">
+            @if(!empty($ttd_hsr))
+                <img src="{{ $ttd_hsr }}" alt="Tanda Tangan HSR" style="height: 62px; max-width: 200px; display: block; margin: 0 auto;" />
+            @else
+                <span style="display: block; height: 62px;">&nbsp;</span>
+            @endif
+        </td>
+    </tr>
+    <tr>
+        <td align="center" style="padding: 0; text-align: center;">
+            @include('pdf.partials.signature_paren', ['name' => $nama_ttd_hsr ?? null, 'align' => 'center'])
+        </td>
+    </tr>
+</table>
--- resources/views/pdf/partials/signature_paren.blade.php
+++ resources/views/pdf/partials/signature_paren.blade.php
@@ -1,10 +1,10 @@
 @php
     $displayName = trim($name ?? '');
 @endphp
-<table border="0" cellspacing="0" cellpadding="0" align="center" style="border-collapse: collapse;">
+<table border="0" cellspacing="0" cellpadding="0" style="width: 100%; border-collapse: collapse;">
     <tr>
         <td style="padding: 0; vertical-align: bottom; white-space: nowrap;">(</td>
-        <td style="width: 200px; min-width: 200px; text-align: center; padding: 0 4px; vertical-align: bottom;">
+        <td style="width: 184px; text-align: center; padding: 0 4px; vertical-align: bottom;">
             @if($displayName !== '')
                 {{ $displayName }}
             @else
